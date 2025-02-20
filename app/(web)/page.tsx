0230dfc20e8172eb9c6d625c8a60fe432384825b
RCR-Support <soporte@rcrsupport.cl>
2025-02-20 13:57:23 -0300

    scroll mobile menu E

--- app/(web)/page.tsx
+++ app/(web)/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
       <div className="flex justify-center items-center text-4xl ">
-        <h1>Pagina de inicio del Home D</h1>
+        <h1>Pagina de inicio del Home E</h1>
       </div>
       );
 }
