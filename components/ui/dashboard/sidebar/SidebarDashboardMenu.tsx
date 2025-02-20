afab6652b511e52431ca53793d6a4dc432a4e73c
RCR-Support <soporte@rcrsupport.cl>
2025-02-19 22:47:48 -0300

    scroll mobile menu B

--- components/ui/dashboard/sidebar/SidebarDashboardMenu.tsx
+++ components/ui/dashboard/sidebar/SidebarDashboardMenu.tsx
@@ -12,28 +12,27 @@ import { useUIStore } from "@/store/ui/ui-store";
 import { useWindowSize } from "@/hooks/useWindowSize";
 
 export const SidebarDashboardMenu = () => {
-    const closeMenu = useUIStore((state) => state.closeSideMenu)
-    const { data: session } = useSession()
-    const isAdmin = ( session?.user?.role === "admin" )
-
+    const closeMenu = useUIStore((state) => state.closeSideMenu);
+    const { data: session } = useSession();
+    const isAdmin = (session?.user?.role === "admin");
     const { width } = useWindowSize();
-
     const router = usePathname();
 
     const handleClick = () => {
         if (width !== undefined && width <= 1023) { // Ajusta este valor según tu definición de LG
-            closeMenu();
+            setTimeout(closeMenu, 300); // Agregar un retraso para permitir el desplazamiento
         }
     };
+
     return (
         <>
-            <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
+            <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto pointer-events-auto">
                 <div className="flex-1 px-3 bg-white dark:bg-[#282c34] text-slate-600 dark:text-white divide-y space-y-1">
                     <ul className="flex-1 space-y-2">
                         <li className="text-[#757575] dark:text-[#f6f7cf] mb-6">Home</li>
                         <li className={`px-2 py-1
-                            ${router === "/dashboard" 
-                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg text-white " 
+                            ${router === "/dashboard"
+                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg text-white "
                             : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
                         `}>
                             <Link href="/dashboard" className={`font-normal rounded-lg flex items-center p-2 group`}>
@@ -43,8 +42,8 @@ export const SidebarDashboardMenu = () => {
                         </li>
 
                         <li className={`px-2 py-1
-                            ${router === "/" 
-                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg " 
+                            ${router === "/"
+                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg "
                             : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
                         `}>
                             <Link href="/" className="font-normal rounded-lg flex items-center p-2 group justify-between">
@@ -59,8 +58,8 @@ export const SidebarDashboardMenu = () => {
                         </li>
 
                         <li className={`px-2 py-1
-                            ${router === "/reports" 
-                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg " 
+                            ${router === "/reports"
+                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg "
                             : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
                         `}>
                             <div className="font-normal rounded-lg flex items-center p-2 group justify-between">
@@ -74,47 +73,45 @@ export const SidebarDashboardMenu = () => {
                             </div>
                         </li>
 
-                        {
-                            isAdmin &&(
-                                <>
-                                    <li className={`px-2 py-1
-                                        ${router === "/register" 
-                                        ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg " 
-                                        : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
-                                    `}>
-                                        <Link href="/register" className="font-normal rounded-lg flex items-center p-2 group justify-between">
-                                            <div className="flex items-center">
-                                                <FaUserPlus />
-                                                <span className="ml-3">Nuevo Usuario</span>
-                                            </div>
-                                        </Link>
-                                    </li>
+                        {isAdmin && (
+                            <>
+                                <li className={`px-2 py-1
+                                    ${router === "/register"
+                                    ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg "
+                                    : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
+                                `}>
+                                    <Link href="/register" className="font-normal rounded-lg flex items-center p-2 group justify-between">
+                                        <div className="flex items-center">
+                                            <FaUserPlus />
+                                            <span className="ml-3">Nuevo Usuario</span>
+                                        </div>
+                                    </Link>
+                                </li>
 
-                                    <li className={`px-2 py-1
-                                        ${router === "/dashboard/admin" 
-                                        ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg " 
-                                        : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
-                                    `}>
-                                        <Link 
-                                        href="/dashboard/admin" 
+                                <li className={`px-2 py-1
+                                    ${router === "/dashboard/admin"
+                                    ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg "
+                                    : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
+                                `}>
+                                    <Link
+                                        href="/dashboard/admin"
                                         className="font-normal rounded-lg flex items-center p-2 group justify-between"
                                         onClick={handleClick}
-                                        >
-                                            <div className="flex items-center">
-                                                <FaUserPlus />
-                                                <span className="ml-3">Pagina Administrador</span>
-                                            </div>
-                                        </Link>
-                                    </li>
-                                </>
-                            )
-                        }
+                                    >
+                                        <div className="flex items-center">
+                                            <FaUserPlus />
+                                            <span className="ml-3">Página Administrador</span>
+                                        </div>
+                                    </Link>
+                                </li>
+                            </>
+                        )}
                         <Accordion>
-                            <AccordionItem className="px-2  hover:text-[#03c9d7] dark:hover:bg-[#082e45] hover:bg-[#ebf9fa] dark:bg-[#282c34] text-slate-800 dark:text-white" key="1" startContent={<IoIosList/>} aria-label="Dashboard" title="Accordions Title">
+                            <AccordionItem className="px-2 hover:text-[#03c9d7] dark:hover:bg-[#082e45] hover:bg-[#ebf9fa] dark:bg-[#282c34] text-slate-800 dark:text-white" key="1" startContent={<IoIosList />} aria-label="Dashboard" title="Accordions Title">
                                 <ul className="pl-2">
                                     <li className={`px-2 py-1
-                                        ${router === "/subpage1" 
-                                        ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg " 
+                                        ${router === "/subpage1"
+                                        ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg "
                                         : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
                                     `}>
                                         <Link href="/subpage1" className="font-normal rounded-lg flex items-center p-2 group">
@@ -123,8 +120,8 @@ export const SidebarDashboardMenu = () => {
                                         </Link>
                                     </li>
                                     <li className={`px-2 py-1
-                                        ${router === "/subpage2" 
-                                        ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg " 
+                                        ${router === "/subpage2"
+                                        ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg "
                                         : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
                                     `}>
                                         <Link href="/subpage2" className="font-normal rounded-lg flex items-center p-2 group">
@@ -133,8 +130,8 @@ export const SidebarDashboardMenu = () => {
                                         </Link>
                                     </li>
                                     <li className={`px-2 py-1
-                                        ${router === "/subpage3" 
-                                        ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg " 
+                                        ${router === "/subpage3"
+                                        ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg "
                                         : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
                                     `}>
                                         <Link href="/subpage3" className="font-normal rounded-lg flex items-center p-2 group">
@@ -148,8 +145,8 @@ export const SidebarDashboardMenu = () => {
 
                         <li className="text-[#757575] dark:text-[#f6f7cf] mb-6">Estadísticas</li>
                         <li className={`px-2 py-1
-                            ${router === "/analytics" 
-                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg " 
+                            ${router === "/analytics"
+                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg "
                             : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
                         `}>
                             <Link href="/analytics" className={`font-normal rounded-lg flex items-center p-2 group`}>
@@ -158,8 +155,8 @@ export const SidebarDashboardMenu = () => {
                             </Link>
                         </li>
                         <li className={`px-2 py-1
-                            ${router === "/sales" 
-                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg " 
+                            ${router === "/sales"
+                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg "
                             : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
                         `}>
                             <Link href="/sales" className={`font-normal rounded-lg flex items-center p-2 group`}>
@@ -168,8 +165,8 @@ export const SidebarDashboardMenu = () => {
                             </Link>
                         </li>
                         <li className={`px-2 py-1
-                            ${router === "/customers" 
-                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg " 
+                            ${router === "/customers"
+                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg "
                             : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
                         `}>
                             <Link href="/customers" className={`font-normal rounded-lg flex items-center p-2 group`}>
@@ -178,8 +175,8 @@ export const SidebarDashboardMenu = () => {
                             </Link>
                         </li>
                         <li className={`px-2 py-1
-                            ${router === "/products" 
-                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg " 
+                            ${router === "/products"
+                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg "
                             : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
                         `}>
                             <Link href="/products" className={`font-normal rounded-lg flex items-center p-2 group`}>
@@ -188,8 +185,8 @@ export const SidebarDashboardMenu = () => {
                             </Link>
                         </li>
                         <li className={`px-2 py-1
-                            ${router === "/settings" 
-                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg " 
+                            ${router === "/settings"
+                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg "
                             : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
                         `}>
                             <Link href="/settings" className={`font-normal rounded-lg flex items-center p-2 group`}>
@@ -200,8 +197,8 @@ export const SidebarDashboardMenu = () => {
 
                         <li className="text-[#757575] dark:text-[#f6f7cf] mb-6">Finanzas</li>
                         <li className={`px-2 py-1
-                            ${router === "/budget" 
-                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg " 
+                            ${router === "/budget"
+                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg "
                             : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
                         `}>
                             <Link href="/budget" className={`font-normal rounded-lg flex items-center p-2 group`}>
@@ -210,8 +207,8 @@ export const SidebarDashboardMenu = () => {
                             </Link>
                         </li>
                         <li className={`px-2 py-1
-                            ${router === "/expenses" 
-                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg " 
+                            ${router === "/expenses"
+                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg "
                             : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
                         `}>
                             <Link href="/expenses" className={`font-normal rounded-lg flex items-center p-2 group`}>
@@ -220,8 +217,8 @@ export const SidebarDashboardMenu = () => {
                             </Link>
                         </li>
                         <li className={`px-2 py-1
-                            ${router === "/revenue" 
-                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg " 
+                            ${router === "/revenue"
+                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg "
                             : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
                         `}>
                             <Link href="/revenue" className={`font-normal rounded-lg flex items-center p-2 group`}>
@@ -232,8 +229,8 @@ export const SidebarDashboardMenu = () => {
 
                         <li className="text-[#757575] dark:text-[#f6f7cf] mb-6">Recursos Humanos</li>
                         <li className={`px-2 py-1
-                            ${router === "/employees" 
-                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg " 
+                            ${router === "/employees"
+                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg "
                             : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
                         `}>
                             <Link href="/employees" className={`font-normal rounded-lg flex items-center p-2 group`}>
@@ -242,8 +239,8 @@ export const SidebarDashboardMenu = () => {
                             </Link>
                         </li>
                         <li className={`px-2 py-1
-                            ${router === "/payroll" 
-                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg " 
+                            ${router === "/payroll"
+                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg "
                             : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
                         `}>
                             <Link href="/payroll" className={`font-normal rounded-lg flex items-center p-2 group`}>
@@ -252,8 +249,8 @@ export const SidebarDashboardMenu = () => {
                             </Link>
                         </li>
                         <li className={`px-2 py-1
-                            ${router === "/recruitment" 
-                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg " 
+                            ${router === "/recruitment"
+                            ? "bg-[#03c9d7] dark:bg-[#03c9d7] rounded-lg "
                             : "hover:text-[#03c9d7] hover:bg-[#ebf9fa] dark:hover:bg-[#082e45] "}
                         `}>
                             <Link href="/recruitment" className={`font-normal rounded-lg flex items-center p-2 group`}>
@@ -265,5 +262,5 @@ export const SidebarDashboardMenu = () => {
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
